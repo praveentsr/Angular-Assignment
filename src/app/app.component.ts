29e95c2bc685e216972304410e14eabe39366e94
praveentsr <praveen381993s@gmail.com>
2017-12-02 13:53:26 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Clarity } from './clarity';
+import { ClaritydesignService } from './claritydesign.service';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component ({
+   selector: 'app-root',
+   template: '<ul><li>{{data | json}}</li></ul>',
+   providers: [ClaritydesignService]
 })
-export class AppComponent {
-  title = 'app works!';
-}
+
+export   class   AppComponent  {
+   iClarity: Clarity[];
+   data: Object = {};
+   constructor(private _clarity: ClaritydesignService) {
+   }
+   
+   ngOnInit() : void {
+      this._clarity.getclarity().subscribe(claritydata => this.data = claritydata);
+   }
+} 
---
+++ src/app/clarity.ts
@@ -0,0 +1,3 @@
+export interface Clarity {
+    name:string;
+}
